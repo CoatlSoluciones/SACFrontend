3234b1e243c0f19fcccaaf7dbc123cf10a96f39f
Luis Jimenez <wichin@outlook.com>
2023-07-01 20:02:35 -0600

    Add method: ngOninit, getUsersList

--- src/app/modules/usersadmin/users/users.component.ts
+++ src/app/modules/usersadmin/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-users',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class UsersComponent {
 
+  constructor(private usersService: UsersService) { }
+
+  ngOnInit() {
+    this.getUsersList();
+  }
+
+  getUsersList() {
+    this.usersService.getUsersList().subscribe((res: any) => {
+      console.log(res.data);
+    });
+
+  }
+  
 }
