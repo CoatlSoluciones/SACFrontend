bd182141ce97d02f33ae98bfdbda474e0be84e36
Luis Jimenez <wichin@outlook.com>
2023-08-03 07:31:47 -0600

    Add code

--- src/app/modules/players/admin-players/admin-players.component.ts
+++ src/app/modules/players/admin-players/admin-players.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { PlayerService } from 'src/app/services/player.service';
+import { Player } from 'src/interfaces/player';
 
 @Component({
   selector: 'app-admin-players',
   templateUrl: './admin-players.component.html',
   styleUrls: ['./admin-players.component.scss']
 })
-export class AdminPlayersComponent {
+export class AdminPlayersComponent implements OnInit {
+
+  listPlayers: Player[] = [];
+  loading = true;
+  isError = false;
+
+  constructor(private playerService: PlayerService, private _snackbar: MatSnackBar, public dialog: MatDialog) { }
+
+  ngOnInit(): void {
+    this.getPlayerList();
+  }
+
+  getPlayerList() {
+    this.playerService.getPlayerList().subscribe({
+      next: (result: any) => {
+        if (result.success) {
+          this.listPlayers = result.data;
+          console.log(this.listPlayers);
+          this.loading = false;
+          this.isError = false;
+        }
+      },
+      error: (error) => {
+        this.loading = false;
+        this.isError = true;
+      }
+    });
+  }
 
 }
