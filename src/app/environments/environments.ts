f4d0b2444d9474c03794b6e932d0877ef49b40b1
Luis Jimenez <wichin@outlook.com>
2023-08-03 07:25:35 -0600

    Modify environment

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    baseService: 'http://localhost:8080'
+    baseService: 'http://localhost:8081'
     //baseService: 'https://tepesco.serveo.net'
 }
