9aa81f38c90c570daf1d0d950e817deb6b248a37
Luis Jimenez <wichin@outlook.com>
2023-08-03 07:36:04 -0600

    Update code: getPlayerList

--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -1,13 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environments';
+import { Player } from 'src/interfaces/player';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlayerService {
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient) { }
 
+  getPlayerList(): Observable<Player[]> {
+    return this.http.get<Player[]>(`${environment.baseService}/players`);
   }
 
 }
